1064b776c0f7b4d9b49adc6308beb7a505657ca3
Marina DELWAULLE <mdelwaul@e1r9p20.clusters.42paris.fr>
2022-09-24 22:05:10 +0200

    Routes, les getters/setters sont faits, mais la redirection a l'air de raler

--- includes/Routes.hpp
+++ includes/Routes.hpp
@@ -1,19 +1,36 @@
 #ifndef ROUTES_HPP
 # define ROUTES_HPP
+#include <iostream>
+# include <string>
 
 class Routes
 {
 	private:
-		std::string	name;
-		std::string	path;
-		std::string	default_file;
-		bool		auto_index;
-		bool		GET;
-		bool		POST;
-		bool		DELETE;
+		std::string	_name;
+		std::string	_path;
+		std::string	_default_file;
+		bool		_auto_index;
+		bool		_GET;
+		bool		_POST;
+		bool		_DELETE;
 
 	public:
-}
+        std::string getName();
+        void        setName(std::string name);
+        std::string getPath();
+        void        setPath(std::string path);
+        std::string getDefaultFile();
+        void        setDefaultFile(std::string default_file);
+        bool        getAutoindex();
+        void        setAutoindex(bool index);
+        bool        getGET();
+        void        setGET(bool get);
+        bool        getPOST();
+        void        setPOST(bool post);
+        bool        getDELETE();
+        void        setDELETE(bool del);
+};
+
 std::ostream&	operator<<(std::ostream& os, Routes const &route);
 
 #endif
--- srcs/Routes.cpp
+++ srcs/Routes.cpp
@@ -1,14 +1,89 @@
-#include "Server.hpp"
+#include "Routes.hpp"
+#include <iostream>
+#include <string>
+
+
+std::string Routes::getName()
+{
+    return (_name);
+};
+
+void        Routes::setName(std::string name)
+{
+    _name = name;
+};
+
+std::string Routes::getPath()
+{
+    return (_path);
+};
+
+void        Routes::setPath(std::string path)
+{
+    _path = path;
+};
+
+std::string Routes::getDefaultFile()
+{
+    return (_default_file);
+};
+
+void        Routes::setDefaultFile(std::string default_file)
+{
+    _default_file = default_file;
+};
+
+bool        Routes::getAutoindex()
+{
+    return (_auto_index);
+};
+
+void        Routes::setAutoindex(bool index)
+{
+    _auto_index = index;
+};
+
+bool        Routes::getGET()
+{
+    return (_GET);
+};
+
+void        Routes::setGET(bool get)
+{
+    _GET = get;
+};
+
+bool        Routes::getPOST()
+{
+    return (_POST);
+};
+
+void        Routes::setPOST(bool post)
+{
+    _POST = post;
+};
+
+bool        Routes::getDELETE()
+{
+    return (_DELETE);
+};
+
+void        Routes::setDELETE(bool del)
+{
+    _DELETE = del;
+};
+
+
 
 std::ostream&	operator<<(std::ostream& os, Routes const &route)
 {
-    os << "Name : " << routes.getName() << std::endl;
-    os << "Path: " << routes.getPath() << std::endl;
-    os << "Default file : " << routes.getDefaultFile() << std::endl;
-    os << "Autoindex : " << routes.getAutoindex() ? "yes" : "no" << std::endl;
-    os << "GET : " << routes.getGET() ? "allowed" : "forbidden"<< std::endl;
-    os << "GET : " << routes.getPOST() ? "allowed" : "forbidden"<< std::endl;
-    os << "GET : " << routes.getDELETE() ? "allowed" : "forbidden"<< std::endl;
+    os << "Name : " << route.getName() << std::endl;
+    os << "Path: " << route.getPath() << std::endl;
+    os << "Default file : " << route.getDefaultFile() << std::endl;
+    os << "Autoindex : " << (route.getAutoindex() ? "yes" : "no") << std::endl;
+    os << "GET : " << (route.getGET() ? "allowed" : "forbidden") << std::endl;
+    os << "GET : " << (route.getPOST() ? "allowed" : "forbidden") << std::endl;
+    os << "GET : " << (route.getDELETE() ? "allowed" : "forbidden") << std::endl;
 
     return (os);
-}
+};
