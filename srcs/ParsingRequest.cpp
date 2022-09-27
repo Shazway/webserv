25dfb1e436c97d7e44609dce06e5c25fab5841e9
Marina DELWAULLE <mdelwaul@e2r5p4.clusters.42paris.fr>
2022-09-27 22:22:46 +0200

    parsing de la premiere ligne

--- srcs/ParsingRequest.cpp
+++ srcs/ParsingRequest.cpp
@@ -17,7 +17,66 @@ int parsingRequest(HttpRequest &request, char* buffer)
     //si !partiallyCompleted
     if (!request.getPartiallyCompleted())
     {
+		request.setPartiallyCompleted(true);
     //  getline de request line
+        int     i = 0, j = 0;
+        std::string::iterator it = bufferString.begin();
+        while (*it >= "A" && *it <= "Z")
+		{
+			it++;
+			i++;
+		}
+		if (i < 2)
+			return (CODE_NO_METHOD);
+		request.setMethod(bufferString.substr(0, i - 1));
+		while (*it == " ")
+		{
+			it++;
+			i++;
+		}
+		j = i;
+		while (*it != " " && *it != "?")
+		{
+			it++;
+			j++;
+		}
+		if (j - i < 1)
+			return (CODE_NO_PATH);
+		request.setPath(bufferString.substr(i, j - 1));
+		if (*it == "?")
+		{
+			it++;
+			i = j + 1;
+			j = i;
+			while (*it != " ")
+			{
+				it++;
+				j++;
+			}
+			if (j - i < 1)
+				return (CODE_NO_QUERY);
+			request.setQueryString(bufferString.substr(i, j - 1));
+		}
+		while (*it == " ")
+		{
+			it++;
+			j++;
+		}
+		bufferString = bufferString.substr(j, std::string::npos);
+		if (bufferString.find("HTTP") != 0 || bufferString.find('\n') < 8)
+			return (CODE_NO_HTTP_VERSION);
+		j = 0;
+		it = bufferString.begin();
+		while (j < 4 || *it == " ")
+		{
+			j++;
+			it++;
+		}
+		bufferString = bufferString.substr(j, bufferString.find('\n'));
+		if (bufferString.compare("1.0") && bufferString.compare("1.1"))
+			return (CODE_UNSUPPORTED_HTTP_VERSION);
+		request.setHttpVersion(bufferString);
+
     //  getline de header
         
     }
