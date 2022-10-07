9bbcafe9b3ce474f3de409211ae8c967f4d387fd
Marina DELWAULLE <mdelwaul@e1r10p13.clusters.42paris.fr>
2022-10-08 01:48:39 +0200

    boucle sur les sockets finie (a peu pres)

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,6 +2,7 @@
 #include "HttpRequest.hpp"
 #include "Parsing.hpp"
 #include "Server.hpp"
+#include "Socket.hpp"
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -11,6 +12,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/select.h>
+#include <fcntl.h>
+
+//extraire les donnees pertinentes de Server pour les mettre dans sockaddr
+void	init_addr(struct sockaddr *addr, Server serveur);
 
 void	initFdSet(fd_set &fdSet, std::vector<int> *sockets, std::list<int> &fds)
 {
@@ -20,21 +25,21 @@ void	initFdSet(fd_set &fdSet, std::vector<int> *sockets, std::list<int> &fds)
 int	running(std::vector<Server> &servers)
 {
 	int					num = 0; //plus grand fd en utilisation + 1;
-	std::vector<int>	sockets;
-	std::list<int>		fds;
+	std::vector<Socket>	sockets;
+	std::list<Socket>		fds;
+	Socket				socketMaker;
 	fd_set				fdread;
 	fd_set				fdwrite;
 
-	int	s;
-	struct sockaddr		addr;
 	int opt; //ne sert a rien, mais calme le proto de setsockopt
 	for (std::vector<Server>::iterator it = servers.begin(); it != servers.end(); it++)
 	{
-		init_addr(addr, *it);
-		s = socket(/*a remplir*/);
-		bind(s, addr);
-		listen(s, 32);
-		setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+		init_addr(socketMaker.getAddr(), *it);
+		socketMaker.setS(socket(/*a remplir*/));
+		bind(socketMaker.getS(), addr);
+		listen(socketMaker.getS(), 32);
+		setsockopt(socketMaker.getS(), SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+		sockets.push_back(socketMaker);
 	}
 	if (s >= num)
 		num = s + 1;
@@ -44,7 +49,18 @@ int	running(std::vector<Server> &servers)
 		initFdSet(fdread, NULL, fds);
 		if (select(num, &fdread, &fdwrite, 0, 0) < 1)
 			continue ;
-		for (std::)
+		for (std::vector<Socket>::iterator it = sockets.begin(); it != sockets.end(); it++)
+		{
+			if (FD_ISSET((*it).getS(), fdread))
+			{
+				socketMaker.setS(accept((*it).getS(), (*it).getAddr(), sizeof(struct sockaddr)));
+				fcntl(socketMaker.getS(), O_NONBLOCK);
+				//faire un truc avec socketMaker._addr
+				fds.push_back(socketMaker);
+				if (socketMaker.getS() >= num)
+					num = socketMaker.getS() + 1;
+			}
+		}
 	}
 
 }
