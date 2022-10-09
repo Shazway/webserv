347e880a076fb9abde79f8bd2e907db2e8ad8366
Telli MORAGLIS <tmoragli@e1r5p11.clusters.42paris.fr>
2022-10-09 16:33:21 +0200

    Ip and port parsed correctly

--- srcs/parsing/parse_elems.cpp
+++ srcs/parsing/parse_elems.cpp
@@ -6,7 +6,7 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 19:36:47 by tmoragli          #+#    #+#             */
-/*   Updated: 2022/10/08 18:13:59 by tmoragli         ###   ########.fr       */
+/*   Updated: 2022/10/09 16:30:50 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,43 @@ bool	set_port(v_str & args, Server& serv)
 {
 	if (args.size() != 1)
 		return (false);
-	serv.setPort(args[0]);
+	std::string	arg = args.front();
+
+	for (std::string::iterator it = arg.begin(); it != arg.end(); it++)
+		if (!isdigit((*it)))
+			return (false);
+	serv.setPort(arg);
 	return (true);
 }
+
+bool	parse_ip(std::string ip)
+{
+	std::vector<std::string>	split_ip;
+
+	ft_split(ip, split_ip, ".");
+	if (split_ip.size() < 4)
+		return (false);
+	for (v_str_it it = split_ip.begin(); it != split_ip.end(); it++)
+	{
+		std::string tmp = (*it);
+		if (tmp.size() < 1 || tmp.size() > 3)
+			return (false);
+	}
+	return (true);
+}
+
 bool	set_ip(v_str & args, Server& serv)
 {
 	if (args.size() != 1)
 		return (false);
-	serv.setIp(args[0]);
+	std::string	arg = args.front();
+
+	for (std::string::iterator it = arg.begin(); it != arg.end(); it++)
+		if (!isdigit((*it)) && (*it) != '.')
+			return (false);
+	if (!parse_ip(arg))
+		return (false);
+	serv.setIp(arg);
 	return (true);
 }
 bool	set_name(v_str & args, Server& serv)
