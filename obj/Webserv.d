9051c118b86f5c28dd6cdae6b71087dbe653e97e
Telli MORAGLIS <tmoragli@e1r5p11.clusters.42paris.fr>
2022-11-15 20:31:39 +0100

    Added signal handler for sigint

--- obj/Webserv.d
+++
@@ -1,3 +0,0 @@
-obj/Webserv.o: srcs/Webserv.cpp includes/Webserv.hpp includes/Server.hpp \
- includes/Colors.hpp includes/MethodTree.hpp includes/IndexTree.hpp \
- includes/RedirectTree.hpp includes/HttpRequest.hpp includes/Upload.hpp
