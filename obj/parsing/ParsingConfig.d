06f7a5a6336ab01a9678678ee452ce1b799d524a
Nathan CABA <ncaba@e2r2p20.clusters.42paris.fr>
2022-11-15 18:37:27 +0100

    Des chats partout

---
+++ obj/parsing/ParsingConfig.d
@@ -0,0 +1,4 @@
+obj/parsing/ParsingConfig.o: srcs/parsing/ParsingConfig.cpp \
+ includes/Parsing.hpp includes/HttpRequest.hpp includes/Upload.hpp \
+ includes/Server.hpp includes/Colors.hpp includes/MethodTree.hpp \
+ includes/IndexTree.hpp includes/RedirectTree.hpp includes/Server.hpp
---
+++ obj/parsing/ft_split.d
@@ -0,0 +1,3 @@
+obj/parsing/ft_split.o: srcs/parsing/ft_split.cpp includes/Server.hpp \
+ includes/Colors.hpp includes/MethodTree.hpp includes/IndexTree.hpp \
+ includes/RedirectTree.hpp
