c09fb209e39a050c66676fe8822b121c75be8bcc
Nathan CABA <ncaba@e3r2p15.clusters.42paris.fr>
2022-11-18 21:03:21 +0100

    page de validation fast as fuck

--- obj/CgiHandler.d
+++
@@ -1,4 +0,0 @@
-obj/CgiHandler.o: srcs/CgiHandler.cpp includes/CgiHandler.hpp \
-  includes/HttpRequest.hpp includes/Upload.hpp includes/Server.hpp \
-  includes/Colors.hpp includes/MethodTree.hpp includes/IndexTree.hpp \
-  includes/RedirectTree.hpp includes/Webserv.hpp
--- obj/trees/MethodTree.d
+++
@@ -1,4 +0,0 @@
-obj/trees/MethodTree.o: srcs/trees/MethodTree.cpp includes/Colors.hpp \
-  includes/MethodTree.hpp includes/Parsing.hpp includes/HttpRequest.hpp \
-  includes/Upload.hpp includes/Server.hpp includes/IndexTree.hpp \
-  includes/RedirectTree.hpp includes/utilsTree.hpp
